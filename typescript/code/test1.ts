670a196020076a86073430b9739e6be20e64e02a
dinghuahua <huahuajxnu@163.com>
2019-04-03 16:22:57 +0800

    ts

--- typescript/code/test1.ts
+++ typescript/code/test1.ts
@@ -10,10 +10,6 @@ let p: Named;
 // OK, because of structural typing
 p = new Person();
 
-interface Named {
-    name: string;
-}
-
 let x: Named;
 // y's inferred type is { name: string; location: string; }
 let y = { name: 'Alice', location: 'Seattle' };
